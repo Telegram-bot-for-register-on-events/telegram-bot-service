afe41d28b7c1594bb73333630f4bdf4886b230c0
Recrusion <entertohacking@gmail.com>
2025-12-24 20:45:22 +0300

    fixed logging error and added comments

--- internal/app/grpc/app.go
+++ internal/app/grpc/app.go
@@ -9,12 +9,14 @@ import (
 	"google.golang.org/grpc"
 )
 
+// App описывает gRPC-сервер микросервиса
 type App struct {
 	log        *slog.Logger
 	gRPCServer *grpc.Server
 	port       string
 }
 
+// NewApp конструктор для App
 func NewApp(log *slog.Logger, port string) *App {
 	gRPCServer := grpc.NewServer()
 	event.Register(gRPCServer)
@@ -25,34 +27,40 @@ func NewApp(log *slog.Logger, port string) *App {
 	}
 }
 
+// start запускает gRPC-сервер на указанном порту для прослушивания входящих соединений и обработки запросов
 func (a *App) start() error {
 	log := a.log.With(
 		slog.String("operation", "start gRPC server"),
 		slog.String("port", a.port),
 	)
 
+	// Инициализируем слушателя на указанном порту, используя TCP-соединение
 	listener, err := net.Listen("tcp", ":"+a.port)
 	if err != nil {
-		log.Error("start to gRPC server - %s", err.Error())
+		log.Error("start to gRPC server", err.Error())
 		return fmt.Errorf("start gRPC server - %v", err)
 	}
 
 	log.Info("starting gRPC server", slog.String("address", listener.Addr().String()))
 
+	// Принимаем входящие соединения от слушателя, создавая горутину для каждого из них
+	// Cчитываем запросы и вызываем соответствующие обработчики для них
 	if err = a.gRPCServer.Serve(listener); err != nil {
-		log.Error("start to gRPC server - %s", err.Error())
+		log.Error("start to gRPC server", err.Error())
 		return fmt.Errorf("start gRPC server - %v", err)
 	}
 
 	return nil
 }
 
+// MustStart обёртка для start, при ошибке - паникует
 func (a *App) MustStart() {
 	if err := a.start(); err != nil {
 		panic(err)
 	}
 }
 
+// Stop выполняет GracefulShutdown для gRPC-сервера
 func (a *App) Stop() {
 	a.log.With(slog.String("operation", "stop gRPC server")).Info("stopping gRPC server", slog.String("port", a.port))
 	a.gRPCServer.GracefulStop()
