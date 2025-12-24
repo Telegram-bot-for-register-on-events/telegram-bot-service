b72e1d548168d6722583bebd717a08ce0316dc76
Recrusion <entertohacking@gmail.com>
2025-12-24 21:32:14 +0300

    added comments and changed start app

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -7,16 +7,14 @@ import (
 	"syscall"
 
 	"github.com/Recrusion/telegram-bot-service/internal/app"
-	bot "github.com/Recrusion/telegram-bot-service/internal/bot"
 	"github.com/Recrusion/telegram-bot-service/internal/config"
-	"github.com/Recrusion/telegram-bot-service/internal/database"
-	"github.com/Recrusion/telegram-bot-service/internal/repository"
-	"github.com/Recrusion/telegram-bot-service/internal/service"
 )
 
 func main() {
+	// Инициализируем логгер
 	log := setupLogger()
 
+	// Загружаем конфигурацию
 	cfg, err := config.LoadConfig(log)
 	if err != nil {
 		log.Error("failed to load config", "error", err)
@@ -24,36 +22,21 @@ func main() {
 	}
 	log.Info("config successfully loaded")
 
-	db, err := database.Connect(cfg.DatabasePath, log)
-	if err != nil {
-		log.Error("failed to connect database", "error", err)
-		os.Exit(1)
-	}
-	log.Info("connection to database successfully created")
-	defer database.Close(db, log)
+	// Создаём новый инстанс микросервиса
+	application := app.NewApp(log, cfg)
+	// Запускаем его
+	application.MustStart()
 
-	repo := repository.NewUserRepository(db, log)
-	srvc := service.NewUserService(log, repo)
-
-	b, err := bot.NewBot(log, cfg.TelegramBotToken, srvc)
-	if err != nil {
-		panic(err)
-	}
-	application := app.NewApp(log, cfg.GRPCPort)
-
-	go b.MustStart()
-	go application.GRPCServer.MustStart()
-
+	// Создаём канал для приёма сигналов операционной системы
 	stop := make(chan os.Signal, 1)
+	// Передаём входящие сигналы в канал stop
 	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	// Читаем из канала, пока не придёт соответствующий сигнал
 	<-stop
 
-	log.Info("shutting down...")
-	application.GRPCServer.Stop()
-
-	b.Stop()
 }
 
+// setupLogger инициализирует логгер с JSON-обработчиком
 func setupLogger() *slog.Logger {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	return logger
