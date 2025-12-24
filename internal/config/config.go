8b297da62a9dee28bd2f5743cbe8a643c37ea003
Recrusion <entertohacking@gmail.com>
2025-12-24 21:30:03 +0300

    added info out in log

--- internal/config/config.go
+++ internal/config/config.go
@@ -31,7 +31,8 @@ func LoadConfig(log *slog.Logger) (*Config, error) {
 		log.Error("load config", err.Error())
 		return nil, fmt.Errorf("load config - %v", err)
 	}
-
+	log.Info("environment variables successfully loaded")
+	
 	return &Config{
 		TelegramBotToken: getEnv("TG_BOT_TOKEN", ""),
 		DatabasePath:     getEnv("DSN", ""),
