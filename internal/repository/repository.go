4ae55c1637757633da467036ef4b17ce1bdd48b7
Recrusion <entertohacking@gmail.com>
2025-12-24 19:53:53 +0300

    added comments

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -9,17 +9,20 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+// User описывает данные о пользовователе, необходимые для сохранения
 type User struct {
 	ChatID    int64     `db:"chat_id"`
 	Username  string    `db:"username"`
 	CreatedAt time.Time `db:"created_at"`
 }
 
+// UserRepository описывает объект базы данных
 type UserRepository struct {
 	log *slog.Logger
 	db  *sqlx.DB
 }
 
+// NewUserRepository конструктор для создания UserRepository
 func NewUserRepository(db *sqlx.DB, log *slog.Logger) *UserRepository {
 	return &UserRepository{
 		db:  db,
@@ -27,7 +30,9 @@ func NewUserRepository(db *sqlx.DB, log *slog.Logger) *UserRepository {
 	}
 }
 
+// SaveUserInfo метод для сохранения информации в базе данных
 func (repo *UserRepository) SaveUserInfo(ctx context.Context, chatID int64, username string) error {
+	// Выполняем INSERT-запрос
 	_, err := repo.db.NamedExecContext(ctx,
 		"insert into users (chat_id, username, created_at) values (:chat_id, :username, :created_at) on conflict (chat_id) do nothing",
 		User{
