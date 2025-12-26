f6e5e49fb8600aae49335c5f845ebb9a57cde414
Recrusion <entertohacking@gmail.com>
2025-12-26 16:50:42 +0300

    fixed form log and added const for desc op

--- internal/database/database.go
+++ internal/database/database.go
@@ -8,18 +8,24 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// Константы для описания операций
+const (
+	opConnect         = "database.connect"
+	opCloseConnection = "database.closeConnection"
+)
+
 // Connect открывает соединение с базой данной
-func Connect(dsn string, log *slog.Logger) (*sqlx.DB, error) {
-	db, err := sqlx.Open("postgres", dsn)
+func Connect(driverName, dsn string, log *slog.Logger) (*sqlx.DB, error) {
+	db, err := sqlx.Open(driverName, dsn)
 	if err != nil {
-		log.Error("error database connect", err.Error())
-		return nil, fmt.Errorf("error database connect - %w", err)
+		log.Error("operation", opConnect, err.Error())
+		return nil, fmt.Errorf("%s: %w", opConnect, err)
 	}
 
 	// Проверяем подключение к базе данных, в противном случае возвращаем ошибку
 	if err = db.Ping(); err != nil {
-		log.Error("error database connect", err.Error())
-		return nil, fmt.Errorf("error database connect - %w", err)
+		log.Error("operation", opConnect, err.Error())
+		return nil, fmt.Errorf("%s: %w", opConnect, err)
 	}
 
 	return db, nil
@@ -27,7 +33,7 @@ func Connect(dsn string, log *slog.Logger) (*sqlx.DB, error) {
 
 // Close закрывает соединение с базой данных
 func Close(db *sqlx.DB, log *slog.Logger) {
-	log.With(slog.String("operation", "close connection")).Info("closing the database connection")
+	log.With(slog.String("operation", opCloseConnection)).Info("closing the database connection")
 	if err := db.Close(); err != nil {
 		log.Error("closing database connection", err.Error())
 	}
