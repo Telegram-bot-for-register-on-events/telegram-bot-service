80a7b2408b5409d309f73ce10647664d6ecb98db
Recrusion <entertohacking@gmail.com>
2025-12-26 16:49:04 +0300

    added const for desk op

--- internal/client/event/event.go
+++ internal/client/event/event.go
@@ -10,9 +10,9 @@ import (
 
 // Константы для описания операций
 const (
-	opGetEvents    = "GetEvents"
-	opGetEvent     = "GetEvent"
-	opRegisterUser = "RegisterUser"
+	opGetEvents    = "client.GetEvents"
+	opGetEvent     = "client.GetEvent"
+	opRegisterUser = "client.RegisterUser"
 )
 
 // GetEvents метод для получения всех событий
@@ -21,7 +21,7 @@ func (c *Client) GetEvents(ctx context.Context) ([]*pb.Event, error) {
 	response, err := c.client.GetEvents(ctx, &pb.GetEventsRequest{})
 	if err != nil {
 		c.log.Error("request failed", slog.String("operation", opGetEvents), slog.String("error", err.Error()))
-		return nil, fmt.Errorf("error getting events: %w", err)
+		return nil, fmt.Errorf("%s: %w", opGetEvents, err)
 	}
 	c.log.Info("getting events successfully", slog.Int("count", len(response.Events)), slog.String("operation", opGetEvents))
 	return response.GetEvents(), nil
@@ -32,7 +32,7 @@ func (c *Client) GetEvent(ctx context.Context, eventID string) (*pb.Event, error
 	response, err := c.client.GetEvent(ctx, &pb.GetEventRequest{EventId: eventID})
 	if err != nil {
 		c.log.Error("request failed", slog.String("operation", opGetEvent), slog.String("error", err.Error()))
-		return nil, fmt.Errorf("error getting event: %w", err)
+		return nil, fmt.Errorf("%s: %w", opGetEvent, err)
 	}
 	c.log.Info("getting event successfully", slog.String("event_id", eventID), slog.String("operation", opGetEvent))
 	return response.GetEvent(), nil
@@ -43,7 +43,7 @@ func (c *Client) RegisterUser(ctx context.Context, eventID string, chatID int64,
 	response, err := c.client.RegisterUser(ctx, &pb.RegisterUserRequest{EventId: eventID, ChatId: chatID, Username: username})
 	if err != nil {
 		c.log.Error("request failed", slog.String("operation", opRegisterUser), slog.String("error", err.Error()))
-		return false, fmt.Errorf("error registering user: %w", err)
+		return false, fmt.Errorf("%s: %w", opRegisterUser, err)
 	}
 	c.log.Info("register user on event successfully", slog.String("event_id", eventID), slog.String("username", username), slog.String("operation", opRegisterUser))
 	return response.GetSuccess(), nil
